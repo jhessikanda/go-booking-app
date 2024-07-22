eb6081454b6f12a47624c61c241adc88e84e3d92
Jhessica Kanda <jhessica.kanda@skillshare.com>
2024-07-22 18:15:02 -0300

    feat: add function to simulate sending tickets to user with Sleep

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
+	"time"
 )
 
 var conferenceName = "Go Conference"
@@ -30,6 +31,7 @@ func main() {
 
 		if isValidName && isValidEmail && isValidTicketNumber  {
 			bookTicket(userTickets, firstName, lastName, email)
+			sendTicket(userTickets, firstName, lastName, email)
 
 			firstNames := getFirstNames()
 			fmt.Printf("The first names of our bookings are: %v\n", firstNames)
@@ -107,4 +109,12 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 	fmt.Printf("List of bookings is %v\n", bookings)
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("There are still %v tickets available for %v.\n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("###########################")
+	fmt.Printf("Sending ticket:\n %v \nto email address: %v\n", ticket, email)
+	fmt.Println("###########################")
 }
