d44b5b1b51cc2f13f4092268c73c176ca469c684
Jhessica Kanda <jhessica.kanda@skillshare.com>
2024-07-22 10:31:14 -0300

    refactor: move helper to its own package

--- helper/helper.go
+++ helper/helper.go
@@ -1,8 +1,8 @@
-package main
+package helper
 
 import "strings"
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+func ValidateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) > 1 && len(lastName) > 1
 	isValidEmail := strings.Contains(email, "@")
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
